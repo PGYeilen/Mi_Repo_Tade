9757d0124d432259989b4e7a745856d3829a9f26
PGYeilen <159741860+PGYeilen@users.noreply.github.com>
2024-03-27 01:40:23 -0400

    actualizacion.

---
+++ CC-2023/div_frec.vhdl
@@ -0,0 +1,40 @@
+library ieee ;
+    use ieee.std_logic_1164.all ;
+    use ieee.numeric_std.all ;
+
+entity div_frec is
+    generic(
+        N: integer:= 25000000      ----para que me cumpla la placa con 2Hz;
+    );
+  port (
+    clk: in std_logic;
+    rst: in std_logic;
+    rco: out std_logic;
+    en: in std_logic
+  ) ;
+end div_frec ; 
+
+
+architecture ArchDiv_frec of div_frec is
+signal cout: integer :=0;
+signal otro_rso: std_logic :='0';
+begin
+ process (clk) is
+	begin
+	if clk'event and clk='1' then
+	if rst='1' then
+	cout<=0;
+	otro_rso<='0';
+	elsif en='1' and cout=N then
+	cout<=0;
+	otro_rso<= not(otro_rso);
+	else
+	cout<= cout+1;
+	otro_rso<= otro_rso;
+	end if;
+	end if;
+	end process;
+	rco<=otro_rso;
+	
+	
+end ArchDiv_frec ;
